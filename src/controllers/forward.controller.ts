4249276485eb3612cb7531e63ab7c00c3e94c86b
Enoch Klufio <Enochmk94@gmail.com>
2023-06-19 17:48:04 +0000

    refactor: include default method and headers

--- src/controllers/forward.controller.ts
+++ src/controllers/forward.controller.ts
@@ -4,13 +4,16 @@ import { getLogger } from '../libs/logger';
 import { IRequest } from '../interfaces/interfaces';
 
 const logger = getLogger('ForwardController');
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+};
 
 const forwardController: RequestHandler = async (req, res) => {
   const data = req.body as IRequest;
   const config: AxiosRequestConfig = {
     url: data.requestUrl,
-    headers: data.requestHeaders,
-    method: data.requestMethod,
+    headers: data.requestHeaders || defaultHeaders,
+    method: data.requestMethod || 'POST',
     data: data.requestBody,
   };
 
