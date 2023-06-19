1c2be240f1f3604d39cc212920800c0e6428eb5d
Enoch Klufio <Enochmk94@gmail.com>
2023-06-19 15:44:07 +0000

    fix: add controller to route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import 'express-async-errors';
 import express from 'express';
 import loggerMiddleware from './middlewares/requestLogger.middleware';
+import forwardController from './controllers/forward.controller';
 
 const router = express.Router();
 
 router.use(loggerMiddleware);
+router.all('/forward', forwardController);
 
 export default router;
